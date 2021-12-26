8221ec5a4cb56bc7152306c086ab597cdc63478b
omaj01 <omaj01@vse.cz>
2021-12-26 19:06:29 +0100

    Přidání do EmployeeDAO možnost pro selectování zaměstnanců

--- src/main/java/cz/vse/bagger/DAO/EmployeeDao.java
+++ src/main/java/cz/vse/bagger/DAO/EmployeeDao.java
@@ -1,5 +1,70 @@
 package cz.vse.bagger.DAO;
 
+import cz.vse.bagger.Models.Employee;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class EmployeeDao {
-    //select
+    public static Employee searchEmployee (String empId) throws SQLException, ClassNotFoundException {
+        String selectStmt = "SELECT * FROM employees WHERE employee_id="+empId;
+
+        try {
+            ResultSet resultEmployee = DBUtil.dbExecuteQuery(selectStmt);
+            Employee employee = getEmployeeFromResultSet(resultEmployee);
+            return employee;
+        } catch (SQLException exception) {
+            System.out.println("While searching an employee with " + empId + " id, an error occurred: " + exception);
+            //Return exception
+            throw exception;
+        }
+    }
+    private static Employee getEmployeeFromResultSet(ResultSet resultEmployee) throws SQLException
+    {
+        Employee employee = null;
+        if (resultEmployee.next()) {
+            employee = new Employee();
+            employee.setId_Employee(resultEmployee.getInt("Id_Employee"));
+            employee.setId_Team(resultEmployee.getInt("Id_Team"));
+            employee.setId_Login_Credentials(resultEmployee.getInt("Id_Login_Credentials"));
+            employee.setName(resultEmployee.getString("Name"));
+            employee.setSurname(resultEmployee.getString("Surname"));
+            employee.setPosition(resultEmployee.getString("Position"));
+        }
+        return employee;
+    }
+
+    public static ObservableList<Employee> searchEmployees () throws SQLException, ClassNotFoundException {
+        String selectStmt = "SELECT * FROM employees";
+
+        try {
+            ResultSet resultEmployees = DBUtil.dbExecuteQuery(selectStmt);
+            ObservableList<Employee> employeeList = getEmployeeList(resultEmployees);
+            return employeeList;
+        } catch (SQLException exception) {
+            System.out.println("SQL select operation has been failed: " + exception);
+            throw exception;
+        }
+    }
+
+    private static ObservableList<Employee> getEmployeeList(ResultSet resultEmployees) throws SQLException, ClassNotFoundException {
+        ObservableList<Employee> employeeList = FXCollections.observableArrayList();
+
+        while (resultEmployees.next()) {
+            Employee employee = new Employee();
+            employee = new Employee();
+            employee.setId_Employee(resultEmployees.getInt("Id_Employee"));
+            employee.setId_Team(resultEmployees.getInt("Id_Team"));
+            employee.setId_Login_Credentials(resultEmployees.getInt("Id_Login_Credentials"));
+            employee.setName(resultEmployees.getString("Name"));
+            employee.setSurname(resultEmployees.getString("Surname"));
+            employee.setPosition(resultEmployees.getString("Position"));
+            employeeList.add(employee);
+        }
+        return employeeList;
+    }
+
 }
