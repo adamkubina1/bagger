d17646f015d1d61ce38c1244e7a87aaaa13e26d5
adamkubina1 <adam.kubinaa@gmail.com>
2021-12-27 14:49:20 +0100

    Add global var for team

--- src/main/java/cz/vse/bagger/Controllers/RootLayoutController.java
+++ src/main/java/cz/vse/bagger/Controllers/RootLayoutController.java
@@ -1,10 +1,12 @@
 package cz.vse.bagger.Controllers;
 
 import cz.vse.bagger.Models.Employee;
+import cz.vse.bagger.Models.Team;
 import javafx.scene.control.Alert;
 
 public class RootLayoutController {
     public static Employee loggedEmployee; // Realy unsafe way of doing this
+    public static Team loggedEmployeeTeam;
 
     public static void displayAlert(String title, String header, String description){
         Alert alert = new Alert(Alert.AlertType.ERROR);
