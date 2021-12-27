ea333394f41ae7505cb03395f158acddba8e60ee
adamkubina1 <adam.kubinaa@gmail.com>
2021-12-27 16:14:18 +0100

    Adde open pop ups logic

--- src/main/java/cz/vse/bagger/Controllers/MainPageController.java
+++ src/main/java/cz/vse/bagger/Controllers/MainPageController.java
@@ -9,12 +9,17 @@ import cz.vse.bagger.Models.Issue;
 import cz.vse.bagger.Models.Project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 
@@ -69,5 +74,51 @@ public class MainPageController {
         issueName.setText(selectedIssue.getIssue_Title());
         issuePriority.setText(String.valueOf(selectedIssue.getImportance()));
         issueDescription.setText(selectedIssue.getIssue_Description());
+    }
+
+    public void newProject(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/newProject.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void newIssue(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/newIssue.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void newComment(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/comment.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public void editIssue(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/editIssue.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void openSettings(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/settings.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
