35b1a3e2766e47aeecec34fb45b63981765e53e0
adamkubina1 <adam.kubinaa@gmail.com>
2021-12-27 11:54:40 +0100

    Fixed javafx version

--- src/main/java/cz/vse/bagger/Controllers/LoginController.java
+++ src/main/java/cz/vse/bagger/Controllers/LoginController.java
@@ -1,4 +1,51 @@
 package cz.vse.bagger.Controllers;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+public class LoginController{
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private Button loginButton;
+
+
+
+    public void clickLogin(MouseEvent mouseEvent){
+        String usernameText = username.getText();
+        String passwordText = password.getText();
+
+        if (( usernameText.equals("") || passwordText.equals(""))){
+            RootLayoutController rootLayoutController = new RootLayoutController();
+
+            rootLayoutController.displayAlert("Failed to login", "Missing field", "Please enter your login credentials into all required fields.");
+        } else {
+
+            succesfullLogin();
+        }
+    }
+
+    private void succesfullLogin(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+
+            stage.close();
+
+            Stage newStage = new Stage();
+            newStage.setScene(new Scene(root));
+            newStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/cz/vse/bagger/Controllers/RootLayoutController.java
+++ src/main/java/cz/vse/bagger/Controllers/RootLayoutController.java
@@ -1,4 +1,17 @@
 package cz.vse.bagger.Controllers;
 
+import javafx.scene.control.Alert;
+
 public class RootLayoutController {
+
+    public void displayAlert(String title, String header, String description){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(description);
+
+        alert.showAndWait();
+    }
+
 }
