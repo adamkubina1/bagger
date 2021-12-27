bb5e230772e8d0d6a0050d33ee3a67da384a41d7
omaj01 <omaj01@vse.cz>
2021-12-27 18:37:06 +0100

    Implementace přidání nového komentáře k nějakému issue

--- src/main/java/cz/vse/bagger/Controllers/CommentController.java
+++ src/main/java/cz/vse/bagger/Controllers/CommentController.java
@@ -1,4 +1,36 @@
 package cz.vse.bagger.Controllers;
 
+import cz.vse.bagger.DAO.CommentDao;
+import cz.vse.bagger.Models.Issue;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+
 public class CommentController {
+
+    @FXML ListView<Issue> issues;
+    @FXML TextArea message;
+    @FXML Button sendButton;
+    int id_Issue;
+
+    @FXML
+    private void initialize() throws SQLException, ClassNotFoundException {
+        Issue selectedIssue = issues.getSelectionModel().getSelectedItem();
+        id_Issue = selectedIssue.getId_Issue();
+
+
+    }
+    public void sendMessage() throws SQLException, ClassNotFoundException {
+        CommentDao.insertComment(RootLayoutController.loggedEmployee.getId_Employee(),id_Issue, message.getText());
+        Stage stage = (Stage) sendButton.getScene().getWindow();
+        stage.close();
+    }
+
 }
