c17b1fee304dff094f350b75c0ebc3555d6cb72b
omaj01 <omaj01@vse.cz>
2021-12-27 17:19:17 +0100

    Přizpůsobení list typu Employee na String se jménem a příjmením

--- src/main/java/cz/vse/bagger/Controllers/SettingController.java
+++ src/main/java/cz/vse/bagger/Controllers/SettingController.java
@@ -9,18 +9,25 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingController {
     @FXML TextField user_Info;
     @FXML TextField team_Leader;
-    @FXML ListView<Employee> team_Members;
+    @FXML ListView<String> team_Members;
+    List<Employee> team_Employees = new ArrayList<>();
 
     @FXML
     private void initialize() throws SQLException, ClassNotFoundException {
         user_Info.setText(RootLayoutController.loggedEmployee.getName() + " " + RootLayoutController.loggedEmployee.getSurname());
         Employee leader = EmployeeDao.searchEmployee(RootLayoutController.loggedEmployeeTeam.getId_Leader());
         team_Leader.setText(leader.getName() + " " + leader.getSurname());
-        team_Members.getItems().addAll(EmployeeDao.searchEmployeesByTeamId(RootLayoutController.loggedEmployeeTeam.getId_Team()));
+        team_Employees.addAll(EmployeeDao.searchEmployeesByTeamId(RootLayoutController.loggedEmployeeTeam.getId_Team()));
+        List<String> pom = new ArrayList<>();
+        for (Employee employee : team_Employees){
+            pom.add(employee.getName() + " " + employee.getSurname());
+        }
+        team_Members.getItems().addAll(pom);
     }
-
 }
